1f8660fff67b5a7cd16b6e6c07aba7946a0e3f41
Augusto <127844983+Augusto1012@users.noreply.github.com>
2024-08-29 01:26:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const primeiroNome = "Augusto";
+const sobrenome = "Fortunato";
+const idade = 16;
+const altura = 1.71;
+const cidadeNatal = "Curitiba-PR";
+const massa = 92;
+
+console.log("O meu primeiro nome é:");
+console.log(primeiroNome);
+console.log("O nome da minha familia é:" );
+console.log(sobrenome);
+console.log("minha idade é:");
+console.log(idade);
+console.log("A minha altura atual é:");
+console.log(altura);
+console.log("Sou nascido em:");
+console.log(cidadeNatal);
+console.log("Atualmente meu peso é:");
+console.log(massa);
